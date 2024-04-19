fc6ccfe380c7303ce1e20509019bad79a01e9044
rlNkoo <rolakartur96@gmail.com>
2024-04-19 17:15:29 +0200

    First commit

---
+++ src/main/java/pl/rlnkoo/dailymessageappspringboot/DailyMessageAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.dailymessageappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMessageAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMessageAppSpringBootApplication.class, args);
+	}
+
+}
