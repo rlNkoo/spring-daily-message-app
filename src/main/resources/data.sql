a819e78d5770efeb03cacb21eedf61ec265211f4
rlNkoo <rolakartur96@gmail.com>
2024-04-21 20:59:47 +0200

    Finish tested

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
 INSERT INTO
     devotional (id, quote, author, image_path, content, created_at, updated_at, published_at)
 SELECT
-    1, 'And she shall bring forth a son, and thou shalt call his name JESUS: for he shall save his people from their sins.', 'Matthew 1:21', 'images/1.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_DATE
+    1, 'The only way to do great work is to love what you do.', 'Steve Jobs', 'images/1.jpg', 'This quote from Steve Jobs emphasizes the importance of passion and love for ones work. Jobs believed that when you are truly passionate about what you do, it transcends mere job tasks and becomes a pursuit of excellence. Loving your work fuels motivation, creativity, and perseverance, ultimately leading to the creation of something extraordinary.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_DATE
 WHERE
     NOT EXISTS (SELECT id FROM devotional WHERE id = 1);
 
 INSERT INTO
     devotional (id, quote, author, image_path, content, created_at, updated_at, published_at)
 SELECT
-    2, 'Be careful for nothing; but in every thing by prayer and supplication with thanksgiving let your requests be made known unto God.', 'Philippians 4:6', 'images/2.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_DATE + 1
+    2, 'In the middle of difficulty lies opportunity.', 'Albert Einstein', 'images/1.jpg', 'Albert Einsteins words remind us that challenges often conceal opportunities for growth and advancement. When faced with difficulties, it is easy to become discouraged, but Einstein suggests that within these challenges lie the potential for innovation and progress. By reframing setbacks as opportunities to learn and improve, we can harness their transformative power.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_DATE + 1
 WHERE
     NOT EXISTS (SELECT id FROM devotional WHERE id = 2);
 
 INSERT INTO
     devotional (id, quote, author, image_path, content, created_at, updated_at, published_at)
 SELECT
-    3, 'He shall see of the travail of his soul, and shall be satisfied: by his knowledge shall my righteous servant justify many; for he shall bear their iniquities.', 'Isaiah 53:11', 'images/3.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_DATE + 2
+    3, 'The journey of a thousand miles begins with one step.', 'Lao Tzu', 'images/1.jpg', 'This ancient proverb by Lao Tzu encapsulates the idea that every great accomplishment starts with a single action. It underscores the importance of taking that initial step, no matter how daunting the journey ahead may seem. Each small step forward builds momentum and propels us closer to our goals, reminding us that persistence and perseverance are key on the path to success.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_DATE + 2
 WHERE
     NOT EXISTS (SELECT id FROM devotional WHERE id = 3);
